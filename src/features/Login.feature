093e4c14c5465f14f0f614073670c911441ad3a0
Rahul Balashanmugam <tsca.rahul@outlook.com>
2026-03-08 21:59:00 +0000

    New test case added

--- src/features/Login.feature
+++ src/features/Login.feature
@@ -6,3 +6,10 @@ Scenario: Login Page logged in with correct username and password
   When Enter the correct password
   And Click the login button
   Then Verify user is in Login page
+
+@TA-3
+Scenario: Unsuccessful Login try with wrong password
+  Given Enter the username 
+  And Enter the wrong password
+  When Click the Login button
+  Then Verify the Error message displayed
--- src/step-defenitions/LoginStep.ts
+++ src/step-defenitions/LoginStep.ts
@@ -13,7 +13,7 @@ Given('Enter the correct username', async function () {
 });
 
 When('Enter the correct password', async function () {
-  await pageFixture.page.locator('[data-qa="login-password"]').fill('Rahul1235')
+  await pageFixture.page.locator('[data-qa="login-password"]').fill('Rahul1234')
 });
 
 When('Click the login button', async function () {
@@ -23,4 +23,25 @@ When('Click the login button', async function () {
 Then('Verify user is in Login page', async function () {
   await expect(pageFixture.page.locator('li a:has-text("Logged in as") b')).toHaveText('Rahul');
   console.log("Passed")
+});
+
+
+Given('Enter the username', async function () {
+  
+  await pageFixture.page.goto(url);
+  //await pageFixture.page.getByRole('button', { name: 'Consent' }).click();
+  await pageFixture.page.locator('[data-qa="login-email"]').fill('rahul4321@gmail.com')
+});
+
+When('Enter the wrong password', async function () {
+  await pageFixture.page.locator('[data-qa="login-password"]').fill('Rahul1235')
+});
+
+When('Click the login button', async function () {
+  await pageFixture.page.locator('[data-qa="login-button"]').click();
+});
+
+Then('Verify the Error message displayed', async function () {
+  const errorMessage = this.page.locator('p:has-text("Your email or password is incorrect!")');
+  await expect(errorMessage).toHaveText('Your email or password is incorrect!');
 });
